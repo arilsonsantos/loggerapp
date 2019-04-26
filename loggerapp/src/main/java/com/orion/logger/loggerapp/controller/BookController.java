462594c76539c2bff1e14d41e15e996a9fc38200
Arilson Santos <arilson@msn.com>
2019-04-25 22:58:10 -0300

    Same path => logs

--- loggerapp/src/main/java/com/orion/logger/loggerapp/controller/BookController.java
+++ loggerapp/src/main/java/com/orion/logger/loggerapp/controller/BookController.java
@@ -15,7 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
-@RequestMapping(path = "/pessoa")
+@RequestMapping(path = "/logs")
 public class BookController {
 
     private BookService service;
